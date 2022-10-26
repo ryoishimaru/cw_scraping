3982f09227ee4bf64647aaa136017ab48b43edf7
RyoIshimaru <ryo.ishimaru.kyoto@gmail.com>
2022-10-27 02:18:55 +0900

    Added Lancers crawlers too

--- lancers.py
+++ lancers.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Configurations
 # now = datetime.datetime.now()
-latest_record_csv = 'latest_1000ids.csv'
+latest_record_csv = 'lancers_latest_1000ids.csv'
 latest_jobs = pd.read_csv(latest_record_csv)
 latest_ids = latest_jobs['job_id'].tolist()
 
@@ -17,37 +17,41 @@ gmail.user_name = email
 gmail.password = 'ztbpamtijsfuffcy'
 
 driver = webdriver.Chrome('./chromedriver')
-password = "Tw35dfgcs"
+password = "nDdstCb4Bky?P8zb"
 keywords = ['データ分析', 'BI', 'ダッシュボード', 'API', 'tableau', 'データポータル', 'Looker', 'QuickSight', 'gcp', '機械学習', 'Python', 'データ基盤', 'マーケティング', '画像認識']
 ng_words = ['スマホ', 'コピペ', 'ライター', 'SEO',
             'リスティング', '記事', 'アンケート', 'ロゴの作成',
             'デザイン作成', 'イラスト作成', 'カンタン',
             '簡単！', '女性限定', 'レビュー募集', 'SNSマーケティング',
-            'インフルエンサー'] #keywords to be ignored in job description
+            'インフルエンサー', 'ロゴ制作'] #keywords to be ignored in job description
 
 # login
 try:
-    driver.get('https://crowdworks.jp/login?ref=toppage_hedder')
-    driver.find_element(By.NAME, 'username').send_keys(email)
-    driver.find_element(By.NAME, 'password').send_keys(password)
-    driver.find_element(by=By.CLASS_NAME, value='button-login').click()
+    driver.get('https://www.lancers.jp/user/login?ref=header_menu')
+    driver.find_element(By.NAME, 'data[User][email]').send_keys(email)
+    driver.find_element(By.NAME, 'data[User][password]').send_keys(password)
+    driver.find_element(by=By.ID, value='form_submit').click()
     # Move to '仕事をさがす' page after login suucessfully
-    driver.get('https://crowdworks.jp/public/jobs?category=jobs&order=score&ref=mypage_nav1')
+    # driver.get('https://crowdworks.jp/public/jobs?category=jobs&order=score&ref=mypage_nav1')
 
     new_jobs = {'job_id': [], 'title':[], 'url':[], 'client_name':[], 'price':[], 'keyword': []}
     for k in keywords:
         print(f'Working on {k}...')
-        driver.find_element(By.NAME, 'search[keywords]').send_keys(k)
-        driver.find_element(by=By.CLASS_NAME, value='cw-input_group_button').click()
-        job_ids = driver.find_elements(by=By.XPATH, value='//div[@class="search_results"]/ul/li')
-        urls = driver.find_elements(by=By.XPATH, value='//h3[@class="item_title"]/a')
-        item_titles = driver.find_elements(By.CLASS_NAME, 'item_title')
-        client_names = driver.find_elements(by=By.XPATH, value='//div[@class="client-information"]/span[@class="user-name"]')
-        prices = driver.find_elements(By.CLASS_NAME, 'entry_data_row')
-        for j, t, u, c, p in zip(job_ids, item_titles, urls, client_names, prices):
-            job_id = int(j.get_attribute("data-job_offer_id"))
+        # driver.find_element(By.XPATH, '//div[@class="css-1rj2q6e"]/form/input').send_keys(k)
+        driver.get(f'https://www.lancers.jp/work/search?keyword={k}')
+    #     driver.find_element(by=By.CLASS_NAME, value='cw-input_group_button').click()
+    #     job_ids = driver.find_elements(by=By.XPATH, value='//div[@class="search_results"]/ul/li')
+        urls = driver.find_elements(By.CLASS_NAME, 'c-media__title')
+        item_titles = driver.find_elements(By.CLASS_NAME, 'c-media__title-inner')
+        client_names = driver.find_elements(By.CLASS_NAME, 'c-avatar__note')
+        prices = driver.find_elements(By.CLASS_NAME, 'c-media__job-stats')
+        job_status = driver.find_elements(By.CLASS_NAME, 'c-media__job-time__ttl')
+        for t, u, c, p, s in zip(item_titles, urls, client_names, prices, job_status):
+            job_id = str(u.get_attribute('href').split('/')[-1])
             title_name = t.text
-            if (job_id not in latest_ids) and (any(ext in title_name for ext in ng_words) != True):
+            status = s.text
+            if (job_id not in latest_ids) and (any(ext in title_name for ext in ng_words) != True)\
+                and status != '募集終了' and c.text not in ['Lancers Agent', 'PROsheet', 'パーソルテクノロジースタッフ']:
                 new_jobs['client_name'].append(c.text)
                 new_jobs['job_id'].append(job_id)
                 new_jobs['title'].append(title_name)
@@ -56,11 +60,9 @@ try:
                 new_jobs['keyword'].append(k)
                 print(title_name)
                 print('---')
-        driver.find_element(By.NAME, 'search[keywords]').clear()
+    #     driver.find_element(By.NAME, 'search[keywords]').clear()
 
     new_jobs = pd.DataFrame(new_jobs).drop_duplicates(subset=['job_id'])
-    if len(new_jobs) > 0:
-        new_jobs = new_jobs[~new_jobs['price'].str.contains('募集終了')]
     print(f'{len(new_jobs)} new jobs detected.')
 
     all_jobs = pd.concat([latest_jobs, new_jobs]).drop_duplicates(subset=['job_id'])
@@ -74,7 +76,7 @@ finally:
 
 if len(new_jobs) > 0:
     gmail.send(
-        subject="New cw jobs added",
+        subject="New lancers jobs added",
         sender=email,
         receivers=[email],
         html="""
